1b081b596939538bc5f138279118e0bad3b2c9fc
JAVVA <JAVVA@192.168.1.72>
2023-11-07 16:54:14 +0530

    COMMIT

---
+++ core_java/src/com/tns/Encapsulation/EncapsulationDemo.java
@@ -0,0 +1,14 @@
+package com.tns.Encapsulation;
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Encapsulation obj = new Encapsulation();
+		obj.serialno=1;
+		obj.name="kavya";
+		obj.age=21;
+		obj.show();
+	}
+
+	}
